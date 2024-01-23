384788491f262983fee01f39fb3ba6143d95a2f6
Green Apple <greenapple@greenbook.local>
2024-01-23 21:01:33 +0530

    add ga

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 
 import '@/styles/globals/globals.scss';
 import StyledComponentsRegistry from "@/components/registry";
+import AnalyticsProvider from '@/lib/analytics';
  
 
 export default function RootLayout({ children }) {
@@ -9,7 +10,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
+          <AnalyticsProvider>
             {children}
+          </AnalyticsProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
